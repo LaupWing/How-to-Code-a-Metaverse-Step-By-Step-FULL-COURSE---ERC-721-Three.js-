9d8cb98f561afdada674926bcf3a0b978de041b0
LaupWing <loc_strict@hotmail.com>
2022-04-20 18:07:18 +0200

    chai compoennts

--- test/land.test.js
+++ test/land.test.js
@@ -1,5 +1,9 @@
 const Land = artifacts.require('./Land')
 
+require('chai')
+   .use(require('chai-as-promised'))
+   .should()
+
 contract('Land', ()=>{
    const NAME = "Dapp U Buildings"
    const SYMBOL = "DUB"
@@ -14,7 +18,29 @@ contract('Land', ()=>{
    describe('Deployment', ()=>{
       it('returns the contract name', async ()=>{
          result = await land.name()
-         
+         result.should.equal(NAME)
+      })
+      it('returns the symbol', async ()=>{
+         result = await land.symbol()
+         result.should.equal(SYMBOL)
+      })
+      it('returns the cost to mint', async ()=>{
+         result = await land.cost()
+         result.toString().should.equal(COST)
+      })
+      it('returns the max supply', async ()=>{
+         result = await land.maxSupply()
+         result.toString().should.equal('5')
+      })
+      it('returns the contract name', async ()=>{
+         result = await land.getBuildings()
+         result.should.equal(5)
+      })
+   })
+
+   describe('Minting', ()=>{
+      describe('Success', ()=>{
+         land.mint(1, {owner1, value: })
       })
    })
 })
