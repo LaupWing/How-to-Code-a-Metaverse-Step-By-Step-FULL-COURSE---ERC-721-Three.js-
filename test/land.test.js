689bcf6e18ba71679b4f7aa698b1304f33a935f7
LaupWing <loc_strict@hotmail.com>
2022-04-20 18:32:50 +0200

    local

--- test/land.test.js
+++ test/land.test.js
@@ -4,7 +4,9 @@ require('chai')
    .use(require('chai-as-promised'))
    .should()
 
-contract('Land', ()=>{
+const EVM_REVERT = 'VM Exception while processing transaction: revert'
+
+contract('Land', ([owner1, owner2])=>{
    const NAME = "Dapp U Buildings"
    const SYMBOL = "DUB"
    const COST = web3.utils.toWei('1', 'ether')
@@ -40,7 +42,62 @@ contract('Land', ()=>{
 
    describe('Minting', ()=>{
       describe('Success', ()=>{
-         land.mint(1, {owner1, value: })
+         beforeEach(async()=>{
+            result = await land.mint(1, {from: owner1, value: COST})
+         })
+         it('should updates the owner address', async ()=>{
+            result = await land.ownerOf(1)
+            result.should.equal(owner1)
+         })
+         it('updates building details', async ()=>{
+            result = await land.getBuilding(1)
+            result.owner.should.equal(owner1)
+         })
+      })
+
+
+      describe('Failure', ()=>{
+         it('prevents mint with 0 value', async ()=>{
+            await land.mind(1, {from: owner1, value:0}).should.be.rejectedWith(EVM_REVERT)
+         })
+
+         it('prevents mint with invalid ID', async ()=>{
+            await land.mind(100, {from: owner1, value:COST}).should.be.rejectedWith(EVM_REVERT)
+         })
+
+         it('prevents minting if already owned', async ()=>{
+            await land.mind(1, {from: owner1, value:COST})
+            await land.mind(1, {from: owner2, value:COST}).should.be.rejectedWith(EVM_REVERT)
+         })
+      })
+   })
+   describe('Transfers', ()=>{
+      describe('success', ()=>{
+         beforeEach(async ()=>{
+            await land.mint(1, {from: owner1, value: COST})
+            await land.approve(owner2, 1, { from: owner1})
+            await land.transferFrom(owner1, owner2)
+         })
+
+         it('updates the owner address', async ()=>{
+            result = await land.ownerOf(1)
+            result.should.equal(owner2)
+         })
+
+         it('updates building details', async ()=>{
+            result = await land.getBuilding(1)
+            result.should.equal(owner2)
+         })
+      })
+      describe('failure', ()=>{
+         it('prevents transfers without ownership', async ()=>{
+            await land.transferFrom(owner1, owner2, 1, {from:owner2}).should.be.rejectedWith(EVM_REVERT)
+         })
+
+         it('prevents transfers without approval', async ()=>{
+            await land.mint(1, {from: owner1, value:COST})
+            await land.transferFrom(owner1, owner2, 1, {from:owner2}).should.be.rejectedWith(EVM_REVERT)
+         })
       })
    })
 })
